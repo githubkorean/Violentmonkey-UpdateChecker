d6e05accb4b608d33b4583a41fad4e7202f71a61
githubkorean <100678096+githubkorean@users.noreply.github.com>
2024-10-21 23:24:24 +0900

    Update 0.0.user.js

--- Scripts/0.0.user.js
+++ Scripts/0.0.user.js
@@ -1,9 +1,11 @@
 // ==UserScript==
-// @name          업데이트 테스트 0.0
+// @name          업데이트 테스트 0.1
 // @namespace     https://github.com/githubkorean/Violentmonkey-UpdateChecker
+// @supportURL    https://github.com/githubkorean/Violentmonkey-UpdateChecker
+// @homepageURL   https://github.com/githubkorean/Violentmonkey-UpdateChecker
 // @match         *://*/*
-// @version       0.0
-// @description   테스트용 0.0버전 스크립트
+// @version       0.1
+// @description   테스트용 0.1버전 스크립트
 // @icon          https://www.google.com/s2/favicons?sz=256&domain_url=github.com
 // @author        mickey90427 <mickey90427@naver.com>
 // @require       https://github.com/githubkorean/Violentmonkey-UpdateChecker/raw/refs/heads/main/Scripts/Updater.js
